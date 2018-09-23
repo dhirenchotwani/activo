3e446408611c4227bce513e73c514a849eb8e331
akkidawesome <akkidawesome@gmail.com>
2018-09-24 01:04:14 +0530

    adding raw html pages

---
+++ assets/UI components/includes/sidebar.php
@@ -0,0 +1,32 @@
+<div class="sidebar">
+    <nav class="sidebar-nav">
+        <ul class="nav">
+            <li class="nav-item">
+                <a class="nav-link" href="../index.php">
+                    <i class="nav-icon icon-speedometer"></i> Dashboard
+                </a>
+            </li>
+            <li class="nav-title">Theme</li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">
+                    <i class="nav-icon icon-plus"></i>Add Request</a>
+            </li>
+<!--
+            <li class="nav-item">
+                <a class="nav-link" href="#">
+                    <i class="nav-icon icon-pencil"></i>Acknowledgement</a>
+            </li>
+-->
+            <li class="nav-item">
+                <a class="nav-link" href="#">
+                    <i class="nav-icon icon-page"></i>Request History</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">
+                    <i class="nav-icon icon-chart" aria-hidden="true"></i>Reports
+                    </a>
+            </li>
+        </ul>
+    </nav>
+    <button class="sidebar-minimizer brand-minimizer" type="button"></button>
+</div>
